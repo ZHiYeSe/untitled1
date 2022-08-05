83067ff573798334bdf03f848dab40533782f6fc
zhangsan <zhangsan@163.com>
2022-08-05 16:42:24 +0800

    003

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("周末了");
         System.out.println("不想上班");
+        System.out.println("还是早点上班吧");
     }
 }
